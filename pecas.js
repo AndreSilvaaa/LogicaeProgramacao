8119d53bda703cc9576af8c348e5757858f310cf
Andre Silva <smorfeticanos@gmail.com>
2021-12-21 16:02:52 -0300

    Aula concluida

--- pecas.js
+++ pecas.js
@@ -1,6 +1,27 @@
 //commits
 let pesoPeca = 100
+
 if (pesoPeca >= 100){
     console.log ("Peso maior que 100g – Cadastro autorizado!")
+    }
+else {console.log ("Peso inferior a 100g – Cadastro negado!")
 }
-else {console.log ("Peso inferior a 100g – Cadastro negado")}
+
+let numerodePecas = 12
+
+if(numerodePecas <=10) {
+    console.log ("Ainda há espaço na caixa – Cadastro autorizado!")
+}else {
+    console.log ("Caixa sem espaço – Cadastro negado!!")
+}
+
+let nomePeca = "Di"
+
+console.log ("O comprimento do nome da peça é:", nomePeca.length)
+
+if(nomePeca.length < 3){
+    console.log("Impossivel cadastro")
+}
+else{
+    console.log("Cadastro realizado com sucesso")
+}
