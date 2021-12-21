5dde727d4973c0685a2dccf065bdbe3e32eedc23
Andre Silva <smorfeticanos@gmail.com>
2021-12-21 14:47:21 -0300

    Primeiros passos em programação

---
+++ pecas.js
@@ -0,0 +1,6 @@
+//commits
+let pesoPeca = 100
+if (pesoPeca >= 100){
+    console.log ("Peso maior que 100g – Cadastro autorizado!")
+}
+else {console.log ("Peso inferior a 100g – Cadastro negado")}
